0709da44f10219360c02216356bf3fbd7183ab19
soma.matsuda <swfh2367@yahoo.co.jp>
2019-10-12 10:40:37 +0000

    private refix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,10 +47,14 @@ class TasksController < ApplicationController
     flash[:success]="タスクが削除されました"
     redirect_to tasks_url
   end
+  
+  private
+
+  def task_params
+    params.require(:task).permit(:content)
+  end
+
 end
 
 private
 
-def task_params
-  params.require(:task).permit(:content)
-end
