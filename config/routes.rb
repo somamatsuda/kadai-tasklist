78cdba1196da52813012ab18dba0c51fff700096
soma.matsuda <swfh2367@yahoo.co.jp>
2019-11-08 11:55:55 +0000

    user new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  
   get 'toppages/index'
   root to: "toppages#index"
+  
+  get "signup",to: "users#new"
+  resources :users, only: [:index, :show, :new, :create]
   
   resources :tasks
 end
